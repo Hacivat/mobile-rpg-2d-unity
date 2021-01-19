0115c81aa4256299b01c3c4c2f841b40f0a364e9
Osman Pekaydin <pekaydin580@gmail.com>
2021-01-20 02:13:41 +0300

    training ui

---
+++ Assets/Game/Scripts/Behaviours/TrainingButtonBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingButtonBehaviour : MonoBehaviour
+{
+    private StatBehaviour _stat;
+
+    private void Start()
+    {
+        _stat = transform.parent.Find("val").GetComponent<StatBehaviour>();
+    }
+
+    private void Train()
+    {
+
+    }
+}
